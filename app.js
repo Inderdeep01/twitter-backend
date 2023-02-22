4f61c3d4f893b8bf21f172361008b2935d1c8f35
Preetinder0401 <preetinder0548.be20@chitkara.edu.in>
2023-02-22 10:41:54 +0530

    Error handling

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.use('/',(req,res)=>{
     res.status(200).send("app.js is running");
 })
 
+app.use('/',(req,res)=>{
+    res.status(401).send("app.js is not running");
+})
+
 module.exports=app;
