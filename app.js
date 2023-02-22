0bb78919725b692a3311a2fc880aab8168f8140f
inderdeep01 <inderdeepsidhu01@gmail.com>
2023-02-22 11:39:01 +0530

    m1.1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
-
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://malwablockpbx1:<password>@cluster0.ruri3eo.mongodb.net/?retryWrites=true&w=majority`)
 app.use('/',(req,res)=>{
     res.status(200).send("app.js is running");
 })
