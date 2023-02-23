685086582bc8fa5a1381083ea9d4e5c92fd88261
inderdeep01 <inderdeepsidhu01@gmail.com>
2023-02-23 11:21:20 +0530

    get post

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express=require('express');
 const app=express();
 const dotenv = require('dotenv')
 dotenv.config()
+const tweetHandler = require('./routes/tweets')
+//body parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGOOSE)
     .then(()=>console.log("Connected to MonogDB"))
     .catch(err=>console.log(err))
+
+app.use('/tweets',tweetHandler)
 app.use('/',(req,res)=>{
     res.status(200).send("Welcome to Home Route");
 })
--- models/posts.js
+++ models/posts.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-const postSchema=mongoose.Schema({
+
+const postSchema = mongoose.Schema({
     username:{type:String,required:true},
     heading:{type:String},
     description:{type:String}
 })
 
-module.exports=mongoose.model('Posts',postSchema)
+module.exports = mongoose.model('Posts',postSchema)
