4d40bb4757c3786068412d7785cd41d05a77224c
MoneyBatish <money0465.be20@chitkara.edu.in>
2023-03-08 01:58:23 +0530

    Added login Route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,7 +6,7 @@ const User=require("../models/user");
 const genToken = require("../Token");
 const protect = require("../middleware/protect");
 
-const UserSchema=joi.object(
+const SignupSchema=joi.object(
     {
         username:joi.string().min(4).required(),
         email:joi.string().email(),
@@ -18,7 +18,7 @@ const UserSchema=joi.object(
 
 router.post("/signup",async(req,res)=>{
     const {username,email,password,DOB,phone}=req.body;
-    var {error}=await UserSchema.validate({username,email,password,DOB,phone});
+    var {error}=await SignupSchema.validate({username,email,password,DOB,phone});
     if(error){
         res.status(400);
         error=error.details[0].message.replace( /\"/g, "" );
@@ -70,6 +70,59 @@ router.post("/signup",async(req,res)=>{
         ...user,
         token:await genToken(id),
     });
+})
+
+const loginSchema=joi.object({
+    username:joi.string(),
+    email:joi.string().email(),
+    phone:joi.number().min(1000000000).max(9999999999),
+    password:joi.string().required().min(8),
+}).or("username","email","phone");
+
+router.post('/login',async(req,res)=>{
+    const {email,phone,username,password}=req.body;
+    var {error}=await loginSchema.validate({email,phone,username,password});
+    if(error){
+        res.status(400);
+        error=error.details[0].message.replace( /\"/g, "" );
+        return res.json({message:error});
+    }
+    const keyword={
+        $or: [
+            {
+                $and:[
+                    {email:{$eq:email}},
+                    {email:{$ne:null}}
+                ]
+            },
+            {
+                $and:[
+                    {username:{$eq:username}},
+                    {username:{$ne:null}}
+                ]
+            },
+            {
+                $and:[
+                    {phone:{$eq:phone}},
+                    {phone:{$ne:null}}
+                ]
+            }
+        ]
+    }
+    var user=await User.findOne(keyword);
+    if(user && await bcrypt.compare(password,user.password)){
+        const id=user._id;
+        user=user._doc;
+        //deleting unneccesary object properties
+        delete user._id;delete user.password;delete user.createdAt;delete user.updatedAt;delete user.__v;delete user.isAdmin;
+        res.status(200).json({
+            ...user,
+            token:await genToken(id),
+        });
+    }
+    else{
+        res.status(401).json({message:'Invalid Username/Email/Phone Number or Password'});
+    }
 })
 
 
