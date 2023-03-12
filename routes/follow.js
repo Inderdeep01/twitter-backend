854e985558680dc77341cab6835acfe449bb8f13
MoneyBatish <money0465.be20@chitkara.edu.in>
2023-03-13 00:31:22 +0530

    Enable Unfollow Functionality

--- routes/follow.js
+++ routes/follow.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const router=express.Router();
-const protect=require('../middleware/protect')
-router.get('/:username',protect,async(req,res)=>{
+const protect=require('../middleware/protect');
+const User = require('../models/user');
+const Follow=require('../models/follow')
+router.post('/:username',protect,async(req,res)=>{
     const {username}=req.params;
     const user=await User.findOne({username});
+    if(req.user.username==username)
+    {
+        return res.status(401).json({message:'You cannot follow yourself'})
+    }
     if(user.isBanned)
     {
         return res.status(401).json({message:'You Cannot Follow This User'});
@@ -23,3 +29,20 @@ router.get('/:username',protect,async(req,res)=>{
     res.status(201).json(follow);
 })
 
+router.delete('/:username',protect,async(req,res)=>{
+    const {username}=req.params;
+    const user=await User.findOne({username});
+    if(user.isBanned)
+    {
+        return res.status(401).json({message:'This User Is Banned'});
+    }
+    var follow=await Follow.findOne({user:req.user._id,follow:user._id});
+    if(follow){
+        await follow.remove();
+        return res.status(201).json({message:`Unfollows Successfully ${username}`});
+    }
+    res.status(401);
+    throw new Error(`You never follows ${username}`);
+})
+
+module.exports=router;
